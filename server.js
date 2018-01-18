ae13a4fb14c00dc43a3c8d2a491d89e5d4eefb84
cloudswindow <yiqun_wang@126.com>
2018-01-18 15:15:28 +0800

    提交从master合并过来的server.js

---
+++ server.js
@@ -0,0 +1,47 @@
+const Koa = require('koa');
+const path = require('path')
+const webpack = require('webpack');
+const convert = require('koa-convert')
+const views = require('koa-views')
+const koaWebpackMiddleware = require('koa-webpack-middleware');
+const webpackDevMiddleware = koaWebpackMiddleware.devMiddleware;
+const webpackHotMiddleware = koaWebpackMiddleware.hotMiddleware;
+const config = require('./webpack.dev.conf')
+const app = new Koa()
+
+const compiler = webpack(config)
+const PORT = process.env.PORT || 3000
+
+
+
+app.use(require('koa-static')(path.join(__dirname, '../dist')));
+app.use(views(path.join(__dirname, '../dist/'), {
+    extension: 'html'
+}));
+const wdm = webpackDevMiddleware(compiler, {
+    watchOptions: {
+        aggregateTimeout: 300,
+        poll: true
+    },
+    reload: true,
+    publicPath: config.output.publicPath,
+    stats: {
+        colors: true
+    }
+})
+app.use(convert(wdm))
+app.use(convert(webpackHotMiddleware(compiler)))
+const server = app.listen(PORT, 'localhost', (err) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    console.log(`HMR Listening at http://localhost:${PORT}`)
+})
+process.on('SIGTERM', () => {
+    console.log('Stopping dev server')
+wdm.close()
+server.close(() => {
+    process.exit(0)
+})
+})
